e76911484586ddcfc8f486c2b259b17cf959d02f
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-08 23:30:21 +0600

    feat: create group routing and auth pages

---
+++ src/app/(auth)/auth/signin/page.jsx
@@ -0,0 +1,10 @@
+const SignInPage = () => {
+
+    return (
+        <div className="w-full min-h-screen flex items-center justify-center">
+            <h1>Sign-in Page</h1>
+        </div>
+    );
+};
+
+export default SignInPage;

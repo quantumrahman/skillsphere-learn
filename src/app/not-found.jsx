763e851d5939b71b798bdc763efd1fd1aa3f73e4
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-08 22:57:25 +0600

    feat: create loading and not found page component

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+
+    return (
+        <div className="w-full min-h-screen flex items-center justify-center">
+            <h1>Page Not Found</h1>
+        </div>
+    );
+};
+
+export default NotFound;

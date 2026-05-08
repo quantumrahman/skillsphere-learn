11bef73fa7ea478eae55fa02553b16d763c59ea4
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-08 23:19:54 +0600

    feat: create group routing and add main pages

---
+++ src/app/(main)/layout.jsx
@@ -0,0 +1,25 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+
+const geist = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"]
+});
+
+const geist_mono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"]
+});
+
+export const metadata = {
+    title: "SkillSphere - Online Learning Platform.",
+    description: "SkillSphere online learing platform."
+};
+
+export default function MainLayout({ children }) {
+    return (
+        <main className={`${geist.variable} ${geist_mono.variable} w-screen min-h-screen antialiased`}>
+            {children}
+        </main>
+    );
+};

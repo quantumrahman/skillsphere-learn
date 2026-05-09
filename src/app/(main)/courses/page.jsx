5297b16b5292f519d9b32e63b6d61082a13e1881
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-09 20:28:13 +0600

    feat: implement courses page heading and paragraph with search input field design

--- src/app/(main)/courses/page.jsx
+++ src/app/(main)/courses/page.jsx
@@ -1,8 +1,31 @@
+import { RiSearchLine } from "@remixicon/react";
+
 const CoursesPage = () => {
 
     return (
-        <div className="w-full min-h-screen flex items-center justify-center">
-            <h1>Courses Page</h1>
+        <div className="w-full min-h-[calc(100vh-405px)] pt-[65px] px-5">
+            <div className="w-full max-w-[1300px] mx-auto mt-[120px]">
+                <div className="flex flex-col gap-6">
+                    <div className="flex flex-col gap-4">
+                        <h1 className="text-[32px] font-semibold text-white leading-10">Find the Right Course for You</h1>
+                        <p className="text-base font-normal text-[#8e8e8e]">Browse curated courses from industry experts and start building skills that matter in real-world projects.</p>
+                    </div>
+                    <div className="w-full relative">
+                        <input 
+                            id="search" 
+                            type="text" 
+                            placeholder="Search courses..." 
+                            name="search" 
+                            aria-label="input" 
+                            autoComplete="off" role="search"
+                            className="w-full py-3.5 pl-12 bg-[#111111] rounded-md border border-[#ffffff17] text-sm font-normal text-white placeholder:text-[#8e8e8e] focus:outline-none focus:border-[#ffffff24]"
+                        />
+                        <div className="absolute top-1/2 left-4 -translate-y-1/2">
+                            <RiSearchLine size={20} color="#8e8e8e" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
